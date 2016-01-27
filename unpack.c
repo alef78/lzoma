2fc104cf566518d6be9e5d6f01b73b8af86bcdf6
alef78 <alef78@yandex.ru>
2016-01-27 19:28:58 +0400

    Block size in unpacker is no longer static

--- unpack.c
+++ unpack.c
@@ -3,6 +3,7 @@
 // License: GPL v2 or later
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
@@ -17,11 +18,10 @@
   #endif
 #endif
 
-#define byte unsigned char
 #include "lzoma.h"
 
-byte in_buf[HISTORY_SIZE]; /* text to be encoded */
-byte out_buf[HISTORY_SIZE];
+uint8_t *in_buf; /* text to be decoded */
+uint8_t *out_buf;/* decoded text + history */
 
 //#define getbit (((bits=bits&0x7f? bits+bits :  (((unsigned)(*src++))<<1)+1)>>8)&1)
 #define getbit ((bits=bits&0x7fffffff? (resbits=bits,bits+bits) :  (src+=4,resbits=*((uint32_t *)(src-4)),(resbits<<1)+1)),resbits>>31)
@@ -85,7 +85,7 @@ getcode_doneit: \
 getlen_0bit: ;\
 }
 
-static void unpack_c(int history_size, byte *src, byte *dst, byte *start, int left) {
+static void unpack_c(int history_size, uint8_t *src, uint8_t *dst, uint8_t *start, int left) {
   int ofs=-1;
   int len;
   uint32_t bits=0x80000000;
@@ -137,7 +137,7 @@ uselastofs:
 }
 
 #ifdef ASM_X86
-extern unsigned int unpack_x86(byte *src, byte *dst, int left);
+extern unsigned int unpack_x86(uint8_t *src, uint8_t *dst, int left);
 #endif
 
 #include "e8.h"
@@ -148,18 +148,31 @@ int main(int argc,char * argv[]) {
 
   ifd=open(argv[1],O_RDONLY|O_BINARY);
   ofd=open(argv[2],O_WRONLY|O_TRUNC|O_CREAT|O_BINARY,511);
-  int history_size = 0;
+  int current_history = 0;
   int ofs = 0;
   int use_e8=0;
   uint8_t header[8];
   read(ifd,header,8);
-  // TODO: validate header here
-  
+  if (header[0] != (AuthorID >> 8) ||
+      header[1] != (AuthorID & 0xFF) ||
+      header[2] != AlgoID[0] ||
+      header[3] != AlgoID[1] ||
+      header[4] != AlgoID[2] ||
+      header[5] != AlgoID[3] ||
+      header[6] != Version) {
+    fprintf(stderr, "Unsupported compressed data format\n");
+    return 1;
+  }
+  int block_size = 32*1024 << (header[7] & 0xF);
+  int history_size = block_size << 4;
+  in_buf = (uint8_t *)malloc(block_size);
+  out_buf = (uint8_t *)malloc(history_size); // history is 16*block_size
+
   while(read(ifd,&n,4)==4) {
     //if (use_e8) e8(out_buf,n_unp);
     read(ifd,&n_unp,4);
     /*
-    if (n != n_unp && !history_size) 
+    if (n != n_unp && !current_history) 
       read(ifd,&use_e8,1);
     else
       use_e8 = 0;
@@ -174,7 +187,7 @@ int main(int argc,char * argv[]) {
 #error Asm version not yet updated for recent format changes. Please use C version right now.
       unpack_x86(in_buf, out_buf, n_unp);
 #else
-      unpack_c(history_size, in_buf, out_buf+ofs, out_buf, n_unp);
+      unpack_c(current_history, in_buf, out_buf+ofs, out_buf, n_unp);
 #endif
       //tsc=(long unsigned)__rdtsc()-tsc;
       //printf("tsc=%lu\n",tsc);
@@ -182,10 +195,10 @@ int main(int argc,char * argv[]) {
       write(ofd,out_buf+ofs,n_unp);
     }
     ofs+=n_unp;
-    ofs &= (HISTORY_SIZE-1);
-    history_size += n_unp;
-    if (history_size > HISTORY_SIZE-BLOCK_SIZE)
-      history_size = HISTORY_SIZE-BLOCK_SIZE;
+    ofs &= (history_size-1);
+    current_history += n_unp;
+    if (current_history > history_size-block_size)
+      current_history = history_size-block_size;
   }
 
   close(ifd);
