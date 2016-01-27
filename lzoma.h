6ba9a67e024625346fa303dc0a079293bd137a93
alef78 <alef78@yandex.ru>
2016-01-27 21:19:08 +0400

    Updated constants

--- lzoma.h
+++ lzoma.h
@@ -1,12 +1,21 @@
+#define AuthorID 0xA1Ef
+#define AlgoID "LZOM"
+#define Version 0x00
+
+// TODO: get rid of this, use uint8_t
 #define byte unsigned char
-#define MAX_SIZE 16*1024*1024
-#define NEXT_SIZE 1*1024*1024
+
+// TODO: these should become variables
+#define HISTORY_SIZE 16*1024*1024
+#define BLOCK_SIZE (HISTORY_SIZE >> 4)
 
 #define longlen 5400
 #define hugelen 0x060000
 #define breaklz 512
 #define lzmagic 0x02aFFe00*2
 #define lzshift(top) ((9*top)>>3)
+
+// TODO: this parameters should depend on block size
 #define lzlow(total) ((total <= 49549) ? 80 : (total <= 652630) ? 60 :48)
 
 #define breaklen 4
